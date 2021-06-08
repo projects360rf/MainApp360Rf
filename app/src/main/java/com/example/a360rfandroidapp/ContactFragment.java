e8d8da61471f9317bdf10c077822b777fdcfa51d
Kussagra Pathak <11251kushagra@gmail.com>
2021-06-09 03:17:36 +0530

    Contact Us Page

--- app/src/main/java/com/example/a360rfandroidapp/ContactFragment.java
+++ app/src/main/java/com/example/a360rfandroidapp/ContactFragment.java
@@ -1,19 +1,139 @@
 package com.example.a360rfandroidapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.a360rfandroidapp.R;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ContactFragment extends Fragment {
+
+    TextInputEditText nameEt,emailEt,phoneEt,subjectEt,messageEt,placeEt;
+    Button sendMsg;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_contact, container, false);
+        View root = inflater.inflate(R.layout.fragment_contact, container, false);
+
+        nameEt=root.findViewById(R.id.name);
+        emailEt=root.findViewById(R.id.email);
+        phoneEt=root.findViewById(R.id.phone);
+        subjectEt=root.findViewById(R.id.subject);
+        messageEt=root.findViewById(R.id.message);
+        sendMsg=root.findViewById(R.id.sendMsg);
+        placeEt=root.findViewById(R.id.place);
+        Toolbar toolbar = (Toolbar) getActivity().findViewById(R.id.toolbar);
+        toolbar.setTitle("Contact Us");
+        final RelativeLayout progressLayout=root.findViewById(R.id.progressLayout);
+        final CardView formLayout=root.findViewById(R.id.form);
+        sendMsg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String name = nameEt.getText().toString().trim();
+                final String email = emailEt.getText().toString().trim();
+                final String phone = phoneEt.getText().toString().trim();
+                final String subject = subjectEt.getText().toString().trim();
+                final String message = messageEt.getText().toString().trim();
+                final String place=placeEt.getText().toString().trim();
+                if (name.equals("")) {
+                    nameEt.setError("Name cannot be empty");
+                } else if (email.equals("")) {
+                    emailEt.setError("E-mail cannot be empty");
+                } else if (phone.equals("")) {
+                    phoneEt.setError("Phone number cannot be empty");
+                } else if (subject.equals("")) {
+                    subjectEt.setError("Subject cannot be empty");
+                } else if (message.equals("")) {
+                    messageEt.setError("Message cannot be empty");
+                } else if (place.equals("")) {
+                    placeEt.setError("Place cannot be empty");
+                } else if (phone.length() != 10) {
+                    phoneEt.setError("Invalid Number");
+                } else {
+                    progressLayout.setVisibility(View.VISIBLE);
+                    formLayout.setVisibility(View.INVISIBLE);
+                    StringRequest stringRequest = new StringRequest(Request.Method.POST, "https://script.google.com/macros/s/AKfycbxpSYC3Gq3TGWgJYzBALQXiWcKvONBSZFjrWjmjmZ0_lID2rkub/exec", new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            if (response.equals("Success")) {
+                                nameEt.setText("");
+                                emailEt.setText("");
+                                phoneEt.setText("");
+                                messageEt.setText("");
+                                subjectEt.setText("");
+                                placeEt.setText("");
+                                progressLayout.setVisibility(View.INVISIBLE);
+                                formLayout.setVisibility(View.VISIBLE);
+                                Log.i("message", response);
+                                View view=root.getRootView();
+                                Snackbar snackbar=Snackbar.make(view,"SENT",Snackbar.LENGTH_INDEFINITE);
+                                snackbar.setAction("OKAY", new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        snackbar.dismiss();
+                                    }
+                                });
+                                snackbar.show();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            progressLayout.setVisibility(View.INVISIBLE);
+                            formLayout.setVisibility(View.VISIBLE);
+                            Toast.makeText(getActivity().getApplicationContext(),error.toString(),Toast.LENGTH_SHORT).show();
+                            Log.i("ErrorMsg", error.toString());
+                        }
+                    }) {
+                        @Override
+                        protected Map<String, String> getParams() {
+                            Map<String, String> params = new HashMap<>();
+                            params.put("action", "addItem");
+                            params.put("name", name);
+                            params.put("email", email);
+                            params.put("phone", phone);
+                            params.put("subject", subject);
+                            params.put("message", message);
+                            params.put("place", place);
+                            return params;
+                        }
+                    };
+                    int socketTimeOut = 50000;
+                    RetryPolicy retryPolicy = new DefaultRetryPolicy(socketTimeOut, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+                    stringRequest.setRetryPolicy(retryPolicy);
+
+                    RequestQueue queue = Volley.newRequestQueue(getActivity().getApplicationContext());
+                    queue.add(stringRequest);
+                }
+            }
+        });
+
+        return root;
     }
 }
--- app/src/main/java/com/example/a360rfandroidapp/MainActivity.java
+++ app/src/main/java/com/example/a360rfandroidapp/MainActivity.java
@@ -72,52 +72,37 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 Toast.makeText(getApplicationContext(), "institution", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.activity:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        new ActivityFragment()).commit();
-                Toast.makeText(getApplicationContext(), "activity", Toast.LENGTH_SHORT).show();
-                break;
             case R.id.join:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        new JoinFragment()).commit();
-                Toast.makeText(getApplicationContext(), "join", Toast.LENGTH_SHORT).show();
+            case R.id.profile:
+                Toast.makeText(getApplicationContext(), "Feature To Be Added Soon", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.contact:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new ContactFragment()).commit();
-                Toast.makeText(getApplicationContext(), "contact", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.profile:
-                Toast.makeText(getApplicationContext(), "Feature To Be Added Soon", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.about:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new AboutFragment()).commit();
-                Toast.makeText(getApplicationContext(), "about", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.nav_missionVision:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new MissionVisionFragment()).commit();
-                Toast.makeText(getApplicationContext(), "Mission and Vision", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.nav_choose:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new ChooseFragment()).commit();
-                Toast.makeText(getApplicationContext(), "Why Choose 360?", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.nav_founder:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new FounderFragment()).commit();
-                Toast.makeText(getApplicationContext(), "Founder", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.nav_body:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new BodyFragment()).commit();
-                Toast.makeText(getApplicationContext(), "Body", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.nav_board:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new BoardFragment()).commit();
-                Toast.makeText(getApplicationContext(), "Board", Toast.LENGTH_SHORT).show();
                 break;
 
         }
