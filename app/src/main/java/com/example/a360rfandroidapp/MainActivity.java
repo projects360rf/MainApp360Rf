2150176d3e3022858ebba7b57d7c06774356993a
YashDev <allindiaeditor123@gmail.com>
2021-06-09 17:40:55 +0530

    Make Directory

--- app/src/main/java/com/example/a360rfandroidapp/MainActivity.java
+++ app/src/main/java/com/example/a360rfandroidapp/MainActivity.java
@@ -6,14 +6,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.example.a360rfandroidapp.Department.DepartmentFragment;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
